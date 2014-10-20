b6cd9ff5aed13c5247fcfd20b1253476f75ea854
Zach Hoekstra <zhoekstra@gmail.com>
2014-10-20 11:25:03 -0600

    Added RuleType to help with generating rules and remove instanceof calls.

---
+++ src/rules/common/RuleType.java
@@ -0,0 +1,22 @@
+package rules.common;
+
+public enum RuleType {
+    Not,
+    
+    And,
+    Or,
+    Xor,
+    
+    Above,
+    NotAbove,
+    
+    Adjacent,
+    NotAdjacent,
+    
+    LeftOf,
+    NotLeftOf,
+    
+    Range,
+    
+    Other,
+}
